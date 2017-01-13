ee0d04f0fe124d2c20e99a48d53718b606f66fe3
Chris Panda <chrispandabear41@gmail.com>
2017-01-13 15:40:01 -0800

    Bug with merge.

--- viral.creep.action.harvesting.js
+++ viral.creep.action.harvesting.js
@@ -1,7 +1,6 @@
 module.exports = {
 	isValidTarget: function(target){
 	    return (target != null && target.energy != null && target.energy > 0 &&
-	        (target.targetOf === undefined || !_.some(target.targetOf, {'creepType': 'miner'}) ));
 	        (target.targetOf === undefined || target.targetOf.length < target.accessibleFields + 1 /*|| !_.some(target.targetOf, {'creepType': 'miner'})*/ ));
 	},
 };
